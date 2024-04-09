1223bde7e4085d18db803caa16874971b70d3d96
Claas Paaschburg <paaschburgc@gmail.com>
2024-04-09 16:50:06 +0200

    chore: create view "trip overview"

---
+++ src/views/TripOverview.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>Trip.name</h2>
+  <ToggleSwitch labelText="This trip is Public" />
+  <section class="overview">Click "Add Item" to start Herding your Cats</section>
+  <nav>
+    <a href="">View Timeline</a>
+  </nav>
+  <button>Add Item</button>
+  <section id="trip-id">
+    <p>Trip ID:111111</p>
+    <button>Copy ID to Clipboard</button>
+  </section>
+</template>
+
+<script>
+import ToggleSwitch from '@/components/ToggleSwitch.vue'
+export default {
+  components: {
+    ToggleSwitch
+  }
+}
+</script>
+
+<style scoped>
+#trip-id {
+  display: flex;
+}
+.overview {
+  margin: 1rem;
+  min-height: 10rem;
+  background-color: blanchedalmond;
+}
+</style>
