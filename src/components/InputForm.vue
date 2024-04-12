7535200d224b787d68c890161011dc0d63cb4767
Claas Paaschburg <paaschburgc@gmail.com>
2024-04-12 17:35:19 +0200

    style: show input fields as dialog

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,30 +1,46 @@
 <template>
-  <form class="addLodging">
-    <div class="inputtext">
-      <label for="item-name">{{ itemName }}:</label>
-      <input id="item-name" v-model="newLodging.name" placeholder="e. g. Max Mustermann" />
-    </div>
-    <div class="inputtext">
-      <label v-if="isLodging" for="zipcode">Zipcode:</label>
-      <input v-if="isLodging" id="zipcode" v-model="newLodging.endDate" />
-      <label v-if="isLodging" for="city">City:</label>
-      <input v-if="isLodging" id="city" v-model="newLodging.endDate" />
-      <label v-if="isLodging" for="adress">Adress:</label>
-      <input v-if="isLodging" id="adress" v-model="newLodging.endDate" />
-      <label for="begin">Begin:</label>
-      <input type="date" id="begin" v-model="newLodging.startDate" />
-      <label for="end">End:</label>
-      <input type="date" id="end" v-model="newLodging.endDate" />
-      <label v-if="isLodging" for="notes">Notes:</label>
-      <input v-if="isLodging" id="notes" v-model="newLodging.endDate" />
-    </div>
-    <div v-if="isGroupMembers" class="admin">
-      <input type="checkbox" v-model="newLodging.isAdmin" />
-      <label>Admin</label>
-    </div>
-  </form>
-  <footer><button @click.prevent="addLodging">Add new lodging</button></footer>
+  <dialog ref="add-item">
+    <form method="dialog" class="addLodging">
+      <div class="inputtext">
+        <label for="item-name">{{ itemName }}:</label>
+        <input id="item-name" v-model="newLodging.name" placeholder="e. g. Max Mustermann" />
+      </div>
+      <div class="inputtext">
+        <label v-if="isLodging" for="zipcode">Zipcode:</label>
+        <input v-if="isLodging" id="zipcode" v-model="newLodging.endDate" />
+        <label v-if="isLodging" for="city">City:</label>
+        <input v-if="isLodging" id="city" v-model="newLodging.endDate" />
+        <label v-if="isLodging" for="adress">Adress:</label>
+        <input v-if="isLodging" id="adress" v-model="newLodging.endDate" />
+        <label for="begin">Begin:</label>
+        <input type="date" id="begin" v-model="newLodging.startDate" />
+        <label for="end">End:</label>
+        <input type="date" id="end" v-model="newLodging.endDate" />
+        <label v-if="isLodging" for="notes">Notes:</label>
+        <input v-if="isLodging" id="notes" v-model="newLodging.endDate" />
+      </div>
+      <div v-if="isGroupMembers" class="admin">
+        <input id="set-admin" type="checkbox" v-model="newLodging.isAdmin" />
+        <label for="set-admin">Admin</label>
+      </div>
+      <button>Cancel</button>
+    </form>
+  </dialog>
+  <footer>
+    <button @click.prevent="openDialog">Add new {{ itemName }}</button>
+  </footer>
 </template>
+
+<style>
+.inputtext input {
+  margin-bottom: 10px;
+  display: block;
+}
+
+.admin input {
+  margin-right: 5px;
+}
+</style>
 
 <script>
 export default {
@@ -70,6 +86,9 @@ export default {
         this.newLodging.endDate = ''
         this.newLodging.isAdmin = false
       }
+    },
+    openDialog() {
+      this.$refs['add-item'].showModal()
     }
   }
 }
