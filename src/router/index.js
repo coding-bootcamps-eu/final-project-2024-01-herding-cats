a3cf3069ba5a05a64e81fb57d7245aaf84ffa24d
SonjaElena <sonja.koppenhoefer@web.de>
2024-04-13 05:36:38 +0200

    feat: final

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import TripOverview from '@/views/TripOverview.vue'
 import StartPage from '@/views/StartPage.vue'
 import TransportView from '@/views/TransportView.vue'
 import ActivityView from '@/views/ActivityView.vue'
+import LogOut from '@/views/LogOut.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: StartPage
+    },
+    {
+      path: '/LogOut',
+      name: 'logout',
+      component: LogOut
     },
     {
       path: '/notes',
---
+++ src/views/LogOut.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="start-page-main">
+    <h1>
+      Off to corral some kitty chaos.<br />
+      See you around!
+    </h1>
+  </main>
+  <header><img src="@/assets/cat-logo/cat-logo-large.svg" alt="Herding Cats Logotype" /></header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  width: 100vw;
+  height: 25.7rem;
+}
+img {
+  padding: 3rem;
+  margin-left: 1.5rem;
+}
+
+h1 {
+  margin-top: 10rem;
+  color: white;
+  font-weight: 400;
+  text-shadow: 0px 0.2rem 0.2rem rgba(255, 255, 255, 0.25);
+}
+</style>
