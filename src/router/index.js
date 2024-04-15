7f8e5b0fb699b7ab2d588745b517de27c7b26886
SonjaElena <sonja.koppenhoefer@web.de>
2024-04-15 11:13:15 +0200

    feat: vorläufig

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import TripOverview from '@/views/TripOverview.vue'
 import StartPage from '@/views/StartPage.vue'
 import TransportView from '@/views/TransportView.vue'
 import ActivityView from '@/views/ActivityView.vue'
+import LogIn from '@/views/LogIn.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: StartPage
+    },
+    {
+      path: '/LogIn',
+      name: 'login',
+      component: LogIn
     },
     {
       path: '/notes',
