816fe2690da1dbc8d1b359cc89a9bd0c3a02c6b4
SonjaElena <sonja.koppenhoefer@web.de>
2024-04-16 12:32:58 +0200

    feat: password validation in signup and login

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -4,14 +4,26 @@
     <label class="required"> E-mail </label>
     <input type="email" v-model="email" />
 
-    <label class="required"> Password </label>
-    <input type="password" v-model="password" />
+    <div class="info">
+      <label class="required">
+        Password
+        <span class="info-icon" @mouseover="showInfo" @mouseleave="hideInfo">&#9432;</span>
+        <span class="extra-info">
+          Password must contain:
+          <ul>
+            <li>min. 8 characters</li>
+            <li>min. 1 uppercase letter</li>
+            <li>min. 1 lowercase letter</li>
+            <li>min. 1 digit</li>
+          </ul>
+        </span>
+      </label>
+      <input type="password" v-model="password" />
+    </div>
 
-    <router-link :to="{ name: 'alltravels' }">
-      <button class="weiterbtn" @click="logIn">LogIn</button>
+    <router-link @click.prevent="logIn" :to="validation() ? { name: 'alltravels' } : ''">
+      <button class="weiterbtn">LogIn</button>
     </router-link>
-
-    <h3>Welcome back!</h3>
   </div>
 </template>
 
@@ -25,13 +37,30 @@ export default {
   },
 
   methods: {
-    logIn() {}
+    validation() {
+      if (
+        this.email === '' ||
+        this.password.length < 8 ||
+        !/[A-Z]/.test(this.password) ||
+        !/[a-z]/.test(this.password) ||
+        !/\d/.test(this.password)
+      ) {
+        return false
+      } else {
+        return true
+      }
+    },
+    logIn() {
+      if (!this.validation()) {
+        alert(`Please make sure your e-mail address and password are entered correctly.
+    Your password must contain:
+    - min. 8 characters
+    - min. 1 uppercase letter
+    - min. 1 lowercase letter
+    - min. 1 digit`)
+        return
+      }
+    }
   }
 }
 </script>
-
-<style scoped>
-h3 {
-  margin-top: 10rem;
-}
-</style>
