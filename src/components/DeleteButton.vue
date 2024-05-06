14bcf96345ff95aa0ad67429dd3646909c9f2e16
Gustav Hersmann <gustav@gustavhersmann.com>
2024-05-06 13:17:01 +0200

    Feat: Delete Trip converted to a component

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="delete-trip" @click="deleteTrip">Delete Trip</button>
+</template>
+
+<script>
+import { herdingCatsstore } from '@/stores/counter.js'
+export default {
+  name: 'DeleteButton',
+  props: {},
+  data() {
+    return {
+      state: herdingCatsstore()
+    }
+  },
+  methods: {
+    async deleteTrip() {
+      await fetch(`${this.state.apiUrl}events/${this.$route.params.id}`, {
+        method: 'DELETE'
+      })
+      this.$router.push({ name: 'alltravels' })
+    }
+  }
+}
+</script>
+<style scoped>
+.delete-trip {
+  background-color: var(--required-red);
+  margin-top: 2rem;
+}
+</style>
